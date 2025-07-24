c01451e1ed8505dde72b8f966cc501378b9fa90c
mike dupont <h4@solfunmeme.com>
2025-07-24 03:45:36 +0200

    wup

--- crates/ragit-index/src/lib.rs
+++ crates/ragit-index/src/lib.rs
@@ -1,7 +1,6 @@
 pub mod prelude;
+pub use prelude::*;
+
 pub mod agent;
-pub mod chunk_methods;
 pub mod index;
 pub mod query_helpers;
-
-pub use index::{Index, IIStatus, BuildConfig};
--- crates/ragit-index/src/prelude.rs
+++ crates/ragit-index/src/prelude.rs
@@ -1,29 +1,13 @@
-pub use anyhow::Result;
+pub use crate::index::{Index, IIStatus, BuildConfig};
 pub use ragit_api::{Model, ModelRaw, Request, Schema, MuseName, get_model_by_name, MessageContent, JsonType, Pdl, parse_pdl, render_pdl_schema};
-pub use ragit_utils::prelude::*;
-pub use ragit_utils::VERSION;
-
-// External crates that are not covered by ragit_utils::prelude
-pub use serde::{Serialize, Deserialize};
-pub use serde_json::Value;
-pub use tokio::task::JoinSet;
-pub use chrono::Local;
-pub use rust_stemmers::{Stemmer, Algorithm};
-pub use flate2::bufread::{GzDecoder, GzEncoder};
-pub use flate2::Compression;
-pub use strum::IntoEnumIterator;
-pub use tera::Context;
-
-// ragit-types specific imports
-pub use ragit_types::uid::Uid;
-pub use ragit_types::chunk::chunk_struct::Chunk;
-pub use ragit_types::chunk::rendered_chunk::RenderedChunk;
-pub use ragit_uid::load_from_file;
-
-// Internal to ragit-index
-pub use crate::index::index_struct::Index;
-pub use crate::index::load_mode::LoadMode;
-pub use crate::BuildConfig;
-pub use crate::index::tfidf::{TfidfResult, TfidfState, ProcessedDoc};
-pub use crate::index::commands::version::VersionInfo;
-pub use crate::query_helpers::{UidQueryConfig, uid_query};
+pub use ragit_readers::ImageDescription;
+pub use ragit_utils::cli_types::{ArgParser, ArgType, ArgCount, ParsedArgs};
+pub use ragit_model_query_response::{ModelQueryResponse, QueryTurn};
+pub use ragit_types::pdl_types::{Message, MessageContent, Role};
+pub use ragit_types::{ApiError as Error, AuditRecordAt, FileSchema, JsonType, Uid};
+pub use ragit_fs::{exists, create_dir, create_dir_all, join, join3, join4, read_bytes, read_dir, read_string, remove_file, write_bytes, write_string, WriteMode, file_name, parent, extension, get_relative_path, set_extension};
+pub use std::collections::{HashMap, HashSet};
+pub use std::path::{Path, PathBuf};
+pub use std::ops::{Deref, DerefMut};
+pub use lazy_static::lazy_static;
+pub use regex::Regex;
